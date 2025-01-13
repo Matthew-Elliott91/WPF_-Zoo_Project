d9f41ae6abe77439c23cb3cd1851b33850140108
Matthew Elliott <m.elliott091091@gmail.com>
2025-01-13 15:28:18 +0000

    Add project files.

---
+++ WPF - Zoo Project/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+namespace WPF___Zoo_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection sqlConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager
+                .ConnectionStrings["WPF___Zoo_Project.Properties.Settings.PanjutorialsDBConnectionString"].ConnectionString;
+               
+
+            sqlConnection = new SqlConnection(connectionString);
+
+            ShowZoos();
+        }
+
+        private void ShowZoos()
+        {
+            string query = "SELECT * FROM Zoo";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+            using (sqlDataAdapter)
+            { 
+                DataTable zooTable = new DataTable();
+                sqlDataAdapter.Fill(zooTable);
+
+                // What info should be shown in the listbox
+                listZoos.DisplayMemberPath = "Location";
+                //What value should be delivered when an item is selected
+                listZoos.SelectedValuePath = "Id";
+                //The reference to the data the listbox should populate
+                listZoos.ItemsSource = zooTable.DefaultView;
+
+
+            }
+        }
+
+    }
+}
